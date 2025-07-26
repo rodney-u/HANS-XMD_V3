80674151349eec67ca07470bbb65af31ac8d6aeb
rodney-u <rodneyraph4@gmail.com>
2025-07-26 19:53:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "HANS-XMD",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " 263717087382",              
+    OWNER_NAME: process.env.OWNER_NAME || "RODNEY",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " 254745314237",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || 'HANS-XMD',
